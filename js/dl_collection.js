79e24c82ba75065979347b7a8dbf78caa898b602
Asil Cetin <asil.cetin@oeaw.ac.at>
2018-09-18 09:56:31 +0200

    dl collection treeview changes

--- js/dl_collection.js
+++ js/dl_collection.js
@@ -19,6 +19,9 @@ jq2(function( $ ) {
 */
     jq2(document).ready(function() {
 			
+        jq2('#selected_files_size_div').hide();
+        jq2('#collection_dl_info').hide();
+        
         //var uid = Drupal.settings.currentUser;
         var roles = drupalSettings.oeaw.users.roles;
         var actualUserRestriction = 'public';
@@ -40,34 +43,67 @@ jq2(function( $ ) {
         
         var url = jq2('#insideUri').val();
         
+        jq2('#collection_dl_info_button').click(function(e) {
+            if (jq2('#collection_dl_info').css('display') == 'none') {
+                jq2('#collection_dl_info_button').html('Hide usage information');
+                jq2('#collection_dl_info').show(1000);
+            }else{
+                jq2('#collection_dl_info_button').html('Show usage information');
+                jq2('#collection_dl_info').hide(1000);
+            }
+            e.preventDefault();
+            
+        
+        });
+        
+        /**  the collection download input field actions **/
+        jq2("#search-input").keyup(function () {
+            var searchString = $(this).val();
+            $('#collectionBrowser').jstree('search', searchString);
+        });
+        
+        /** the collection download jstree js  **/
         var sumSize = 0;
+        var disableChkArray = [];
         jq2('#collectionBrowser')
             .jstree({
                 core : {
                     data : {
                         "url" : '/browser/get_collection_data/'+url,
-                        "dataType" : "json" 
-                    }
+                        "dataType" : "json"
+                    },
+                    themes : { stripes : true }
 		},
                 checkbox : {
                     //keep_selected_style : true,
                     tie_selection : false,
                     whole_node : false
                 },
-                plugins : [ 'checkbox' ],
+                search: {
+                    case_sensitive: false,
+                    show_only_matches: true
+                },
+                plugins : [ 'checkbox', 'search' ]
 	})
         //handle the node clicking to download the file
         .on("changed.jstree", function (node, data) {
+            jq2('#selected_files_size_div').show();
             jq2('#dl_link').hide();
             jq2('#dl_link_txt').hide();
             if(data.selected.length == 1) {
                 //if we have a directory then do not open the fedora url
                 if(data.node.original.dir === false){
+                    let id = data.instance.get_node(data.selected[0]).id;
                     //check the permissions for the file download
                     var resourceRestriction = data.instance.get_node(data.selected[0]).original.accessRestriction;
                     if( ((resourceRestriction != 'public') &&  resourceRestriction == actualUserRestriction) || actualUserRestriction == 'admin' ){
+                        jq2('#not_enough_permission').hide();
                         window.location.href = data.instance.get_node(data.selected[0]).original.uri;
+                    /*} else if( ((resourceRestriction != 'public') &&  resourceRestriction != actualUserRestriction) && actualUserRestriction != 'admin' ){
+                        //if the user doesnt have any rights on the resource then we will make it unreachable to download
+                        //jq2("#"+id+" > a > i.jstree-checkbox").attr("disabled", true);*/
                     } else if( resourceRestriction == 'public'){
+                        jq2('#not_enough_permission').hide();
                         window.location.href = data.instance.get_node(data.selected[0]).original.uri;
                     }else {
                         alert("Sorry! But you dont have enough rights to download it!");
@@ -75,12 +111,40 @@ jq2(function( $ ) {
                 }
             }
         })
+        .on("loaded.jstree", function (e, d) {
+            jq2.each( d.instance._model.data, function( key, value ) {
+                jq2.each( value.original, function( k, v ) {
+                    if(k == 'accessRestriction') {
+                        if(  ((v != 'public') &&  v != actualUserRestriction) && actualUserRestriction != 'admin'  ){
+                            disableChkArray.push(key+'_anchor');
+                        }
+                    }
+                });
+            });
+        })
+        .on("before_open.jstree", function (e, d) {
+            jq2.each( disableChkArray, function( key, value ) {
+                jq2("#"+value+" > i").removeClass('jstree-icon jstree-checkbox');
+                let text = jq2("#"+value).text();
+                jq2("#"+value).html(text + '');
+                jq2("#"+value).css('color','red');
+            });
+        })
         //handle the checkboxes to download the selected files as a zip
         .on("check_node.jstree", function (node, data) {
+            jq2('#selected_files_size_div').show();
             jq2('#dl_link').hide();
             jq2('#dl_link_txt').hide();
             jq2('#getCollectionData').prop('disabled', false);
-
+            jq2('#not_enough_permission').hide();
+            
+            jq2.each( disableChkArray, function( key, value ) {
+                jq2("#"+value+" > i").removeClass('jstree-icon jstree-checkbox');
+                let text = jq2("#"+value).text();
+                jq2("#"+value).html(text + '');
+                jq2("#"+value).css('color','red');
+            });
+            
             if(data.instance.get_checked(true)) {
                 
                 var formData = data.instance.get_checked(true);
@@ -98,8 +162,7 @@ jq2(function( $ ) {
                     //check the rights
                     if( ((resourceRestriction != 'public') &&  resourceRestriction != actualUserRestriction) && actualUserRestriction != 'admin' ){
                         //if the user doesnt have any rights on the resource then we will make it unreachable to download
-                        $('#'+id+" > a").removeClass();
-                        $('#'+id+" > a").addClass("jstree-anchor jstree-disabled");
+                        jq2('#not_enough_permission').show();
                     }
                     
                     if(size && uri){
@@ -119,9 +182,18 @@ jq2(function( $ ) {
             }
         })
         .on("uncheck_node.jstree", function (node, data) {
+            jq2('#selected_files_size_div').show();
             jq2('#dl_link').hide();
             jq2('#dl_link_txt').hide();
             jq2('#getCollectionData').prop('disabled', false);
+            
+            
+            jq2.each( disableChkArray, function( key, value ) {
+                jq2("#"+value+" > i").removeClass('jstree-icon jstree-checkbox');
+                let text = jq2("#"+value).text();
+                jq2("#"+value).html(text + '');
+                jq2("#"+value).css('color','red');
+            });
             
             if(data.instance.get_checked(true)) {
                 selectedItems = [];
