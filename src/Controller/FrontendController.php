e9cf73716dfecf8bd555796067d1949e67683777
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-26 16:42:21 +0000

    Deploy acdh-oeaw/repo-gui to github.com/acdh-oeaw/repo-gui.git:master

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -700,10 +700,11 @@ class FrontendController extends ControllerBase
             drupal_set_message(t('No').' '.t('Data'), 'error', false);
             return array();
         }
-        $templateData["insideUri"] = $data;
+        
         $identifier = $this->oeawFunctions->detailViewUrlDecodeEncode($data, 0);
         
         $templateData = array();
+        $templateData["insideUri"] = $this->oeawFunctions->detailViewUrlDecodeEncode($data, 1);
         //get the title and the fedora url
         $fdUrlArr = array();
         $fdUrlArr = $this->oeawStorage->getTitleByIdentifier($identifier);
